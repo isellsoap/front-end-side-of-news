1d1e8e89369e8a7c47c3bbb0d7e063366926ccce
Francesco Schwarz <mail@francescoschwarz.de>
2016-05-06 11:30:33 +0200

    Simplify JS.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,31 +2,40 @@
   var mainTable = document.querySelector('.main-table');
   var mainTableTbodyRows = mainTable.querySelectorAll('tbody tr');
 
+  function tbodyCellsofColumn(columnNumber) {
+    var cells = [];
+    for (var i = 0, len = mainTableTbodyRows.length; i < len; i++) {
+      var cell =  mainTableTbodyRows[i].cells[columnNumber];
+      cells.push(cell);
+    }
+    return cells;
+  }
+
   // transform the last time checked dates to relative time (e.g. "3 days ago")
   function timeAgo() {
-    for (var i = 0, len = mainTableTbodyRows.length; i < len; i++) {
-      var cell =  mainTableTbodyRows[i].cells[2];
+    var cells = tbodyCellsofColumn(2);
 
+    cells.forEach(function (cell) {
       if (cell.innerHTML !== '') {
         cell.innerHTML = moment(cell.innerHTML, 'MMMM Do YYYY').fromNow();
       }
-    }
+    });
   }
 
   timeAgo();
 
+  // calculate the average amount of all cells from a certain column
   function averageForColumn(columnNumber) {
+    var cells = tbodyCellsofColumn(columnNumber);
     var sum = 0;
     var average;
     var cellText;
 
-    for (var i = 0, len = mainTableTbodyRows.length; i < len; i++) {
-      var cell =  mainTableTbodyRows[i].cells[columnNumber];
-
+    cells.forEach(function (cell) {
       if (cell.innerHTML !== '') {
         sum += parseFloat(cell.innerHTML);
       }
-    }
+    });
 
     average = Math.round((sum / mainTableTbodyRows.length) * 100) / 100;
     cellText = document.createTextNode(average);
